ef0a19da2e46b86c293e8e9eaaad0ba97b5a0683
igotbitches <check.point9917@yandex.ru>
2021-02-17 15:13:33 +0300

    update

--- src/serverRequest.js
+++ src/serverRequest.js
@@ -1,19 +1,26 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const sendButton = document.getElementById("send"); // button send
+  sendButton.addEventListener("click", function () {
+    let payload =
+      "name=" + encodeURIComponent(document.getElementById("value").value);
 
-    const sendButton = document.getElementById('send') // button send
-    sendButton.addEventListener('click', function () {
-        let payload = 'name=' + encodeURIComponent(document.getElementById('value').value)
+    const request = new XMLHttpRequest();
+    request.open("POST", "http://34.106.108.107:8080/cgi-bin/script.cgi", true);
 
-        const request = new XMLHttpRequest()
-        request.open('POST', 'http://34.106.108.107:8080/cgi-bin/script.cgi', true)
+    request.addEventListener("readystatechange", function () {
+      console.log(parseInt(request.responseText.replace(/[^\d]/g, "")));
+      //   document.querySelector("#img").classList.add("opacity");
+      document.querySelector("#img").style = "background: rgba(0, 0, 0, 0.5)";
 
-        request.addEventListener('readystatechange', function () {
-            console.log(parseInt(request.responseText.replace(/[^\d]/g, '')));
-            document.querySelector("#img").classList.add('opacity');
-            document.querySelector("#result").innerHTML = `${parseInt(request.responseText.replace(/[^\d]/g, ''))} % unique`;
-        })
+      document.querySelector("#result").innerHTML = `${parseInt(
+        request.responseText.replace(/[^\d]/g, "")
+      )} % unique`;
+    });
 
-        request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-        request.send(payload)
-    })
-})
+    request.setRequestHeader(
+      "Content-Type",
+      "application/x-www-form-urlencoded"
+    );
+    request.send(payload);
+  });
+});
